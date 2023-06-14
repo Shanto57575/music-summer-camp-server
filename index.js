5920ba3abe54ea967e55d303f1b989dcdab07633
Shanto57575 <shanto57575@gmail.com>
2023-06-14 23:55:16 +0600

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
